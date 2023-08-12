acf06f6b701a05ab5be77316a48b185ae45e8558
Tarek Aloui <taloui@college.harvard.edu>
2023-08-11 23:01:22 -0400

    added main and scraper to installed_apps and migrated db

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    affiliation = models.CharField(
+        max_length=255, blank=True, null=True
+    )  # Can be blank or null
+    hIndex = models.PositiveIntegerField()
+    i10Index = models.PositiveIntegerField()
+    citations = models.PositiveIntegerField()
+    interests = models.TextField(
+        blank=True, null=True
+    )  # Store interests as comma-separated values
+    profileUrl = models.URLField()
+
+
+class Tweet(models.Model):
+    url = models.URLField()
+    user_name = models.CharField(max_length=255)
+    retweets = models.PositiveIntegerField()
+    likes = models.PositiveIntegerField()
+    text = models.TextField()
+    date = models.DateTimeField()
+
+
+class Paper(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    title = models.CharField(max_length=255)
+    authors = models.ManyToManyField(Author)
+    abstract = models.TextField()
+    pub_date = models.DateTimeField()
+    updated_date = models.DateTimeField()
+    categories = models.TextField(
+        blank=True, null=True
+    )  # Store categories as comma-separated values
+    links = models.TextField(
+        blank=True, null=True
+    )  # Store links as comma-separated values
+    comment = models.TextField(blank=True, null=True)
+    journal_ref = models.CharField(max_length=255, blank=True, null=True)
+    trendiness_score = models.FloatField()
+    relatedTweets = models.ManyToManyField(Tweet)
