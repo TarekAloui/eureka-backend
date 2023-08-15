5d75b959aca202b8563fd5772b74558e46081505
Tarek Aloui <taloui@college.harvard.edu>
2023-08-14 23:39:28 -0400

    added mutations for paper and tweet

--- apps/main/schema.py
+++ apps/main/schema.py
@@ -102,12 +102,121 @@ class DeleteAuthor(graphene.Mutation):
 
 # Similar mutations for Paper and Tweet can be defined here...
 
+class CreatePaper(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        abstract = graphene.String()
+        authors = graphene.List(AuthorType)
+        relatedTweets = graphene.List(TweetType)
+
+    paper = graphene.Field(PaperType)
+
+    def mutate(self, info, title, abstract=None, authors=None, relatedTweets=None):
+        paper = Paper.objects.create(title=title, abstract=abstract, authors=authors, relatedTweets=relatedTweets)
+        return CreatePaper(paper=paper)
+    
+
+class UpdatePaper(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        title = graphene.String()
+        abstract = graphene.String()
+        authors = graphene.List(AuthorType)
+        relatedTweets = graphene.List(TweetType)
+
+    paper = graphene.Field(PaperType)
+
+    def mutate(self, info, id, title=None, abstract=None, authors=None, relatedTweets=None):
+        paper = Paper.objects.get(pk=id)
+        if title:
+            paper.title = title
+        if abstract:
+            paper.abstract = abstract
+        if authors:
+            paper.authors = authors
+        if relatedTweets:
+            paper.relatedTweets = relatedTweets
+        paper.save()
+        return UpdatePaper(paper=paper)
+
+
+class DeletePaper(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    success = graphene.Boolean()
+
+    def mutate(self, info, id):
+        try:
+            paper = Paper.objects.get(pk=id)
+            paper.delete()
+            return DeletePaper(success=True)
+        except Paper.DoesNotExist:
+            return DeletePaper(success=False)
+
+
+class CreateTweet(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        text = graphene.String(required=True)
+        author = graphene.Field(AuthorType)
+        relatedPaper = graphene.Field(PaperType)
+
+    tweet = graphene.Field(TweetType)
+
+    def mutate(self, info, id, text, author=None, relatedPaper=None):
+        tweet = Tweet.objects.create(id=id, text=text, author=author, relatedPaper=relatedPaper)
+        return CreateTweet(tweet=tweet)
+    
+
+class UpdateTweet(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        text = graphene.String()
+        author = graphene.Field(AuthorType)
+        relatedPaper = graphene.Field(PaperType)
+
+    tweet = graphene.Field(TweetType)
+
+    def mutate(self, info, id, text=None, author=None, relatedPaper=None):
+        tweet = Tweet.objects.get(pk=id)
+        if text:
+            tweet.text = text
+        if author:
+            tweet.author = author
+        if relatedPaper:
+            tweet.relatedPaper = relatedPaper
+        tweet.save()
+        return UpdateTweet(tweet=tweet)
+
+
+class DeleteTweet(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    success = graphene.Boolean()
+
+    def mutate(self, info, id):
+        try:
+            tweet = Tweet.objects.get(pk=id)
+            tweet.delete()
+            return DeleteTweet(success=True)
+        except Tweet.DoesNotExist:
+            return DeleteTweet(success=False)
+
+
 # Mutation
 class Mutation(graphene.ObjectType):
     create_author = CreateAuthor.Field()
     update_author = UpdateAuthor.Field()
     delete_author = DeleteAuthor.Field()
-    # Add fields for Paper and Tweet mutations here...
+    create_paper = CreatePaper.Field()
+    update_paper = UpdatePaper.Field()
+    delete_paper = DeletePaper.Field()
+    create_tweet = CreateTweet.Field()
+    update_tweet = UpdateTweet.Field()
+    delete_tweet = DeleteTweet.Field()
+
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
