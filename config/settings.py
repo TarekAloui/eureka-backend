96d43d5829994b71e611c5eebd0d4321af6e5c89
Tarek Aloui <taloui@college.harvard.edu>
2023-08-26 22:42:08 -0400

    deployed backend django to azure

--- config/settings.py
+++ config/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vn=3de-fqvm7!%9jg4%34i$wk9i!r+t!1ra&#_3o@nla)b3oft"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 DEBUG_HOSTS = ['127.0.0.1', 'localhost', 'localhost:3000']
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['eureka-papers-backend.azurewebsites.net']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
