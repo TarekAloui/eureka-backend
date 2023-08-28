fb5a23261b541d5196e38df6b2757db1c7be121c
Tarek Aloui <taloui@college.harvard.edu>
2023-08-27 23:25:37 -0400

    fixed settings to use django-environ, migration works

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Loading environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,10 +84,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+# "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "eureka-papers-db-django",
+        "USER": "eureka_admin",
+        "PASSWORD": env('DB_PASSWORD'),
+        "HOST": env('DB_SERVER_NAME'),
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+        }
     }
 }
 
